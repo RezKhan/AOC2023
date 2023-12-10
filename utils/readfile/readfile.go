a53b55a20423e6b03289b2082af05ed6db2909b5
Rez <probablyrez@gmail.com>
2023-12-10 11:25:48 +1100

    created readfile utility that returns the lines from each file as an array of strings

---
+++ utils/readfile/readfile.go
@@ -0,0 +1,24 @@
+package readfile
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+func ReadFile(filePath string) []string {
+	f, err := os.Open(filePath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer f.Close()
+
+	fScanner := bufio.NewScanner(f)
+	fScanner.Split(bufio.ScanLines)
+	var lines []string
+	for fScanner.Scan() {
+		line := fScanner.Text()
+		lines = append(lines, line)
+	}
+	return lines
+}
