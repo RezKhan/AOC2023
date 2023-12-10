070642b63fb4011c90217cb901afb11b5aa759e2
Rez <probablyrez@gmail.com>
2023-12-10 21:07:54 +1100

    Added the start of the check hand type

--- d07/d07.go
+++ d07/d07.go
@@ -8,52 +8,71 @@ import (
 	"strings"
 )
 
-var cardfaces = map[string]int {
-	"1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7":7, 
+var cardfaces = map[string]int{
+	"1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7,
 	"8": 8, "9": 9, "T": 10, "J": 11, "Q": 12, "K": 13, "A": 14,
 }
 
 type hand struct {
-	cards []string
-	bid int
+	cards    []string
+	bid      int
+	handtype int // based on sequence, 7
+	score    int
 }
 
+// hand types:
+// [ 5 ]			5 of a kind: 5 of one
+// [ 4 1 ]			4 of a kind: 4 of one -> then check higher value order
+// [ 3 2 ]			full house: 3 of one, 2 of another -> then check higher value order
+// [ 3 1 1 ]		3 of a kind: 3 of one, 1 of one, 1 of one -> then check higher value order
+// [ 2 2 1 ]		2 pair: 2 of one, 2 of one, 1 of one -> then check higher value order
+// [ 2 1 1 1 ]		1 pair: 2 of one, 1 of one, 1of one, 1 of one  -> then check higher value order
+// [ 1 1 1 1 1 ]	Only ones -> then check higher value order
 
-// sequences
-// 5 of one
-// 4 of one -> highest first card
-// 3 of one, 2 of another -> highest first card
-// 3 of one, 1 of one, 1 of one -> highest first card
-// 2 of one, 2 of one, 1 of one -> highest first card
-// Only ones -> highest first card
-
-
+func checkHandType(hand hand) {
+	cards := make([]string, len(hand.cards))
+	copy(cards, hand.cards)
+	slices.Sort(cards)
+	log.Println(hand.cards, cards)
+	var sequences []int
+	s := 1
+	for i := 0; i < len(cards); i++ {
+		if i == 0 {
+			sequences = append(sequences, s)
+			continue
+		}
+		if cards[i] == cards[i-1] {
+			s++
+			sequences[len(sequences)-1] = s
+		} else {
+			s = 1
+			sequences = append(sequences, s)
+		}
+	}
+	slices.Sort(sequences)
+	slices.Reverse(sequences)
+	log.Println(sequences)
+}
 
 func partOne(lines []string) {
 	var hands []hand
 
 	for _, line := range lines {
-		var h  hand
-		tmp := strings.Split(line, " ")	
+		var h hand
+		tmp := strings.Split(line, " ")
 		cards := strings.Split(tmp[0], "")
-		bid, err := strconv.Atoi(tmp[1]) 
+		bid, err := strconv.Atoi(tmp[1])
 		if err != nil {
 			log.Println(err)
 		}
 		h.cards = cards
 		h.bid = bid
-		hands = append(hands, h)		
-	}
-	for _, h := range hands {
-		log.Println(h)
-		c := make([]string, len(h.cards))
-		copy(c, h.cards)
-		slices.Sort(c)
-		log.Println(h.cards, c)
+		checkHandType(h)
+		hands = append(hands, h)
 	}
 }
 
-func main()  {
+func main() {
 	filePath := "d07test.txt"
 
 	lines := rf.ReadFile(filePath)
