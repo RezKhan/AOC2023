d0ea717c5ee240034f6de3fb9cfe868bfb5b3aa9
Rez <probablyrez@gmail.com>
2023-12-16 06:36:37 +1100

    Part two

--- d09/d09.go
+++ d09/d09.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	rf "readfile"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -28,6 +29,29 @@ func subHistory(history []int) []int {
 	history = append(history, history[lh-1]+subHist[len(subHist)-1])
 	return history
 }
+
+func PartTwo(lines []string) {
+	var histories [][]int
+	for _, line := range lines {
+		ns := strings.Split(line, " ")
+		var history []int
+		for _, n := range ns {
+			h, err := strconv.Atoi(n)
+			if err == nil {
+				history = append(history, h)
+			}
+		}
+		slices.Reverse(history)
+		histories = append(histories, history)
+	}
+	sumh := 0
+	for _, history := range histories {
+		history := subHistory(history)
+		sumh += history[len(history)-1]
+	}
+	log.Println("Sum of last numbers in history: ", sumh)
+}
+
 
 func PartOne(lines []string) {
 	var histories [][]int
@@ -55,5 +79,6 @@ func main() {
 	filePath := "d09input.txt"
 
 	lines := rf.ReadFile(filePath)
-	PartOne(lines)
+	// PartOne(lines)
+	PartTwo(lines)
 }
